1d96d8667443242d4de8d83bafe3109ca15f5c51
Jaikinator <jaikinator@googlemail.com>
2021-12-02 20:06:54 +0100

    removed self.normalized query to get sure it will be allways get normalized

--- dqc/utils/datastruct.py
+++ dqc/utils/datastruct.py
@@ -40,14 +40,12 @@ class CGTOBasis:
         # Please note that the square of normalized wavefunctions do not integrate
         # to 1, but e.g. for s: 4*pi, p: (4*pi/3)
 
-        # if the basis has been normalized before, then do nothing
-        if self.normalized:
-            return self
 
         coeffs = self.coeffs
 
         # normalize to have individual gaussian integral to be 1 (if coeff is 1)
-        coeffs = coeffs / torch.sqrt(gaussian_int(2 * self.angmom + 2, 2 * self.alphas))
+        if not self.normalized:
+            coeffs = coeffs / torch.sqrt(gaussian_int(2 * self.angmom + 2, 2 * self.alphas))
 
         # normalize the coefficients in the basis (because some basis such as
         # def2-svp-jkfit is not normalized to have 1 in overlap)
